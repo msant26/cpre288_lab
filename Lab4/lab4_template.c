821f83c4713a7fb183e663e96c99516d5b64a2c9
Fennell <owenf718@iastate.edu>
2026-02-24 14:46:19 -0600

    lab 4 finalized

--- Lab4/lab4_template.c
+++ Lab4/lab4_template.c
@@ -19,19 +19,43 @@
 
 
 int main(void) {
+    lcd_init();
+    lcd_printf("Start");
 	button_init();
 	timer_init(); // Must be called before lcd_init(), which uses timer functions
-	lcd_init();
+	cyBot_uart_init();
+
 	            // Don't forget to initialize the cyBot UART before trying to use it
 
 	uint8_t button;
-
+	int i;
+	int length;
 	
 	while(1)
 	{
 
       button = button_getButton();
+      char buffer[30];
+      if (button == 4){
+          sprintf(buffer, "Button 4 is now pressed\r\n");
+      }else if (button == 3){
+          sprintf(buffer, "Button 3 is now pressed\r\n");
+      }else if (button == 2){
+          sprintf(buffer, "Button 2 is now pressed\r\n");
+      }else if (button == 1){
+          sprintf(buffer, "Button 1 is now pressed\r\n");
+      }else {
+          sprintf(buffer, "No button is pressed\r\n");
+      }
       lcd_printf("Button: %d", button);
+
+      length = sizeof(buffer) / sizeof(buffer[0]);
+
+      for(i=0; i<length; i++){
+          cyBot_sendByte(buffer[i]);
+      }
+
+
       timer_waitMillis(200);
 
 
