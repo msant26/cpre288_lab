be1d7d73926c94fb1a9d599d4b507a84e3e24b94
msant26 <msant@iastate.edu>
2026-03-24 14:57:03 -0500

    adc_init

--- Lab8/adc.c
+++ Lab8/adc.c
@@ -8,6 +8,39 @@
 
 void adc_init(){
 
+<<<<<<< HEAD
+=======
+    // Sets up GPIO
+    SYSCTL_RCGCGPIO_R |= 0x02;
+
+    while((SYSCTL_PRGPIO_R & 0x02) == 0){};
+
+    // Enable GPIO functions and analog
+    GPIO_PORTB_AFSEL_R |= 0x10;
+    GPIO_PORTB_DEN_R &= ~0x10;
+    GPIO_PORT_B_AMSEL_R |= 0x10;
+
+    // Enables ADC clock
+    SYSCTL_RCGCADC_R |= 0x01;
+
+    // Enable status busy wait loop
+    while((SYSCTL_PRADC_R & 0x01) == 0){};
+
+    // Disables SS3
+    ADC0_ACTSS_R &= ~0x08;
+
+    // Continously samples SS3
+    ADC0_EMUX_R |= 0xF000;
+
+    // Selects AIN10
+    ADC0_SSMUX3_R = 10;
+
+    ADC0_SSCTL3_R |= 0x06;
+
+    // Enables SS3
+    ADC0_ACTSS_R |= 0x08;
+
+>>>>>>> f5c58f4 (adc_init)
 
 }
 
@@ -24,8 +57,6 @@ uint16_t adc_read(){
     ADC0_ISC_R = 0x08;
 
     return result;
-
-}
 
 }
 
