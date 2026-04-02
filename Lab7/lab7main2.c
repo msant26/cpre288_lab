7c239c52f2f53fc0db448a1334241282c107906f
Santiago <msant@iastate.edu>
2026-04-01 19:00:37 -0500

    .

--- Lab7/lab7main2.c
+++ Lab7/lab7main2.c
@@ -22,7 +22,7 @@
 
 
 int targetAngle;
-int targetDist;
+int targetDist = 1000;
 double distMM;
 
 
@@ -31,7 +31,8 @@ int main(void) {
     oi_t *sensor_data = oi_alloc(); // do this only once at start of main()
     oi_init(sensor_data); // do this only once at start of main()
 
-    scanForThinnest(&targetAngle, &targetDist);
+    while(targetDist > 15){
+        scanForThinnest(&targetAngle, &targetDist);
 
         distMM = (double) targetDist * 10.0;
         if(targetAngle > 90){
@@ -42,6 +43,8 @@ int main(void) {
         }
 
         move_forward(sensor_data, distMM - 100.0);
+    }
+
 
     oi_free(sensor_data); // do this once at end of main()
     return 0;
