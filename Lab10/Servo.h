84a43b1426653671a8af2cb493a18c7c3b4aa000
Santiago <msant@iastate.edu>
2026-04-07 15:05:42 -0500

    .

--- Lab10/Servo.h
+++ Lab10/Servo.h
@@ -11,31 +11,15 @@
 #include <inc/tm4c123gh6pm.h>
 #include "driverlib/interrupt.h"
 
-extern volatile uint32_t pulse_width;
-extern volatile uint8_t done;
-extern volatile uint8_t overflow;
-extern volatile uint32_t interrupt_cnt;
-
 /**
  * Initialize ping sensor. Uses PB3 and Timer 3B
  */
-void ping_init (void);
+void servo_init(void);
 
 /**
  * @brief Trigger the ping sensor
  */
-void ping_trigger (void);
+void servo_move(uint16_t degrees);
 
-/**
- * @brief Timer3B ping ISR
- */
-void TIMER3B_Handler(void);
-
-/**
- * @brief Calculate the distance in cm
- *
- * @return Distance in cm
- */
-float ping_getDistance (void);
 
 #endif /* SERVO_H_ */
